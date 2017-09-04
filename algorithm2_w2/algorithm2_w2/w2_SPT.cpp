1760c64408d7c0430b952c22c559d3257f05a097
EsmeZ <xiaoxuanzhang9@gmail.com>
2017-09-03 21:46:38 -0400

    Build 2D map storing Dijkstra data

---
+++ algorithm2_w2/algorithm2_w2/w2_SPT.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+
+#define NUM_VER 200
+
+using namespace std;
+
+// heap element {edge weight, vertice}
+class HEAP_ELM {
+	int Edge;
+	int Ver;
+public:
+	HEAP_ELM() {};
+	HEAP_ELM(int _Edge, int _Ver){
+		Edge = _Edge;
+		Ver = _Ver;
+	}
+	int getEdge() const { return Edge; }
+	int getVer() const { return Ver; }
+};
+
+// comparator for building min heap
+class HEAP_COMP{
+public: 
+	bool operator() (const HEAP_ELM& v1, HEAP_ELM& v2){
+		return v1.getEdge() > v2.getEdge();
+	}
+};
+
+int main(){
+
+	HEAP_ELM** ver_arr = new HEAP_ELM*[NUM_VER]; // 2d map storing vertices and their connections
+	int* num_conn = new int[NUM_VER]; // 1d array storing number of connections for each vertice
+	int vc = 0; // vertice counter
+
+	string sptfile = "C:\\Users\\Xiaoxuan\\Desktop\\cousera\\algorithm stanford\\course 2\\\\dijkstraData.txt";
+	string line, tmp_ver, tmp_conn, tmp_edge;
+
+	ifstream myfile(sptfile);
+	if (myfile.is_open()){
+		while (getline(myfile, line)){
+			int nc = count(line.begin(), line.end(), '\t') - 1; // get number of connected vertices by counting # of '\t' in the string
+			
+			num_conn[vc] = nc; 
+			ver_arr[vc] = new HEAP_ELM[nc]; // for each vertice, create an array storing connection and edge weight
+
+			replace_if(line.begin(), line.end(), ispunct, ' ');  // replace "," with space
+			stringstream connection(line);
+			connection >> tmp_ver;
+			if (stoi(tmp_ver) != (vc + 1)){ cout << "no!" << endl; };
+
+			for (int i = 0; i < nc; i++){
+				connection >> tmp_conn >> tmp_edge;
+				ver_arr[vc][i] = HEAP_ELM(stoi(tmp_edge), stoi(tmp_conn));
+			}
+			vc++;
+		}
+	}
+
+
+
+	return 0;
+}
