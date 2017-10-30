61e8a8072eacb8f0e39d7de49d4047ba8603632b
EsmeZ <xiaoxuanzhang9@gmail.com>
2017-10-29 21:51:23 -0400

    Solve max-spacing k-clustering (very naive implementation)

--- algorithm3_w2/algorithm3_w2/algorithm3_w2.cpp
+++ algorithm3_w2/algorithm3_w2/algorithm3_w2.cpp
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <set>
 
-#define NUM_CLS 4 
+#define NUM_CLS 4
 using namespace std;
 
 class HEAP_ELM {
@@ -38,9 +38,50 @@ public:
 };
 
 
+
+int Find(int parent[], int i){
+	if (parent[i] == -1){
+		return i;
+	}
+	else {
+		return Find(parent, parent[i]);
+	}
+}
+
+
+void Union(int parent[], int i, int j){
+	int pi = Find(parent, i);
+	int pj = Find(parent, j);
+
+	parent[pi] = pj; 
+}
+
+bool doContinue(int parent[], int N){
+	// return true until only NUM_CLS clusters
+	int num_cls = 0;
+	for (int i = 0; i < N; i++){
+		if (parent[i] == -1)
+			num_cls++;
+	}
+	return num_cls > NUM_CLS;
+}
+
+void isCycle(int parent[], int i, int j){
+	int ph = Find(parent, i);
+	int pt = Find(parent, j);
+
+	if (abs(ph - pt) < std::numeric_limits<double>::epsilon()) {
+		return;
+	}
+	else {
+		Union(parent, ph, pt);
+	}
+}
+
+
 int main() {
 
-	string clsfile = "C:\\Users\\Xiaoxuan\\Desktop\\cousera\\algorithm stanford\\course 3\\w2_clustering_short.txt";
+	string clsfile = "C:\\Users\\Xiaoxuan\\Desktop\\cousera\\algorithm stanford\\course 3\\w2_clustering.txt";
 	string line, ntmp, htmp, ttmp, ctmp;
 	int M = -1, N;
 	multiset<HEAP_ELM, HEAP_COMP> kcluster;
@@ -65,7 +106,39 @@ int main() {
 		}
 	}
 
+	// initialize parent array
+	int* parent = new int[N];
+	for (int i = 0; i < N; i++){
+		parent[i] = -1;
+	}
 
+
+	while (doContinue(parent, N)){
+		int h = kcluster.begin()->GetHead();
+		int t = kcluster.begin()->GetTail();
+
+		kcluster.erase(kcluster.begin());		
+		isCycle(parent, h - 1, t - 1);
+
+	}
+
+
+
+	while (!kcluster.empty()){
+		int h = kcluster.begin()->GetHead();
+		int t = kcluster.begin()->GetTail();
+
+		int ph = Find(parent, h-1);
+		int pt = Find(parent, t-1);
+
+		if (abs(ph - pt) < std::numeric_limits<double>::epsilon()) {
+			kcluster.erase(kcluster.begin());
+		}
+		else {
+			cout << "max spacing: " << kcluster.begin()->GetCost() << endl;
+			break;
+		}
+	}
 
 
 	
