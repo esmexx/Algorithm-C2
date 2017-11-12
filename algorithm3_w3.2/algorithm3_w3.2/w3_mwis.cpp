2fe69f32fbf98058d9b69736be5ad0eebd9d3873
EsmeZ <xiaoxuanzhang9@gmail.com>
2017-11-11 22:45:52 -0500

    Initial implementation of max-weight independent set

---
+++ algorithm3_w3.2/algorithm3_w3.2/w3_mwis.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	string mwisfile = "C:\\Users\\Xiaoxuan\\Desktop\\cousera\\algorithm stanford\\course 3\\w3_mwis_short4.txt";
+	string line, ntmp, wtmp;
+	int ct = -1, num_nodes;
+	long long* node_arr = NULL;
+	bool* node_mwis = NULL;
+
+	ifstream myfile(mwisfile);
+	if (myfile.is_open()){
+		while (getline(myfile, line)) {
+			if (ct == -1) {
+				stringstream connection(line);
+				connection >> ntmp;
+				num_nodes = stoi(ntmp);
+				if (!node_arr) {
+					node_arr = new long long[num_nodes];
+					node_mwis = new bool[num_nodes];
+				}
+				ct++;
+			}
+			else {
+				stringstream connection(line);
+				connection >> wtmp;
+				node_arr[ct] = stoll(wtmp);
+				node_mwis[ct] = false;
+				ct++;
+			}
+		}
+	}
+
+	// Let A be the filled-in array
+	long long* A = new long long[num_nodes];
+	A[0] = 0; A[1] = node_arr[1];
+
+	for (int i = 2; i < num_nodes; i++){
+		A[i] = max(A[i - 1], A[i - 2] + node_arr[i]);
+	}
+
+	// reconstruction algorithm to get max-weight IS
+	int k = num_nodes - 1;
+	long long S = 0;
+
+	while (k > 1) {
+		if (A[k - 1] >= (A[k - 2] + node_arr[k])) {
+			k--;
+		}
+		else {
+			node_mwis[k] = true;
+			S += node_arr[k];
+			k = k - 2;
+		}
+	}
+
+	// properly handle base cases
+	if (node_mwis[3]){
+		node_mwis[1] = true;
+		S += node_arr[1];
+	}
+
+	if (node_mwis[2]){
+		node_mwis[0] = true;
+		S += node_arr[0];
+	}
+	
+
+	int node_in[8] = { 1, 2, 3, 4, 17, 117, 517, 997}; // no vertex 0
+	int node_inb[8] = { 0, 0, 0, 0, 0, 0, 0, 0};
+
+	for (int i = 0; i < 8; i++){
+		if (node_mwis[node_in[i]-1]){
+			node_inb[i] = 1;
+		}
+	}
+
+
+	cout << "max sum: " << S << endl;
+
+	for (int i = 0; i < 8; i++){
+		cout << node_inb[i] << " ";
+	}
+	cout << endl;
+	
+
+	delete[] node_arr;
+	delete[] node_mwis;
+	delete[] A;
+	return 0;
+}
