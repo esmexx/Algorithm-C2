224d2c5814974f01270a2080635ab9654877f3be
EsmeZ <xiaoxuanzhang9@gmail.com>
2017-12-17 11:38:38 -0500

    Speed up using the EBellflaging algorithm

--- algorithm4_w1/algorithm4_w1/w1_apsp.cpp
+++ algorithm4_w1/algorithm4_w1/w1_apsp.cpp
@@ -41,23 +41,30 @@ int BellmanFord(GraphElm* graph, int num_ver, int num_edge, int src){
 
 	// create an array dist[] of size |V| with all values as infinite except dist[src]
 	int* dist = new int[num_ver];
+    int* F = new int[num_ver]; // ref: "Extended Bellman Ford Algorithm with Optimized Time of Computation"
 	for (int i = 0; i < num_ver; i++){
 		dist[i] = INT_MAX;
+        F[i] = 0;
 	}
 	dist[src - 1] = 0;
+    F[src - 1] = 1;
 
 	for (int i = 1; i < num_ver; i++){
 		bool stop = true; // stop early if dist doesn't change
-		for (int j = 0; j < num_ver; j++) { // loop through all edges and update
-			for (int k = 0; k < graph[j].GetNumTails(); k++) {
-				int u = graph[j].GetHead() - 1;
-				int v = graph[j].GetTail(k).first - 1;
-				int cost = graph[j].GetTail(k).second;
-				if (dist[u] != INT_MAX && dist[u] + cost < dist[v]) {
-					dist[v] = dist[u] + cost;
-				    stop = false;
-				}
-			}
+		for (int j = 0; j < num_ver; j++) {
+		    int u = graph[j].GetHead() - 1;
+            if (F[u] == 1) {
+                F[u] = 0;
+			    for (int k = 0; k < graph[u].GetNumTails(); k++) {
+				    int v = graph[j].GetTail(k).first - 1;
+				    int cost = graph[j].GetTail(k).second;
+				    if (dist[u] != INT_MAX && dist[u] + cost < dist[v]) {
+					    dist[v] = dist[u] + cost;
+                        F[v] = 1;
+				        stop = false;
+				    }
+			    }
+            }
 		}
         if (stop) break;
 	}
@@ -72,7 +79,7 @@ int BellmanFord(GraphElm* graph, int num_ver, int num_edge, int src){
 			if (dist[u] != INT_MAX && dist[u] + cost < dist[v]){
 				cout << "Graph contains negative cycle." << endl;
 				delete[] dist;
-
+                delete[] F;
 				return INT_MIN;
 			}
 		}
@@ -80,6 +87,7 @@ int BellmanFord(GraphElm* graph, int num_ver, int num_edge, int src){
 
 	shortest_dist = *min_element(dist, dist + num_ver);
 	delete[] dist;
+    delete[] F;
 
 	return shortest_dist;
 
